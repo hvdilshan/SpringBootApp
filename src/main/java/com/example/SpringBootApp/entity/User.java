8693907c43ef9f3bae2167d83b2801be277f7177
Viranga Dilshan <virangavithanage@gmail.com>
2022-01-21 15:20:54 +0530

    user model creation

--- src/main/java/com/example/SpringBootApp/entity/User.java
+++ src/main/java/com/example/SpringBootApp/entity/User.java
@@ -1,4 +1,19 @@
 package com.example.SpringBootApp.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+
 }
